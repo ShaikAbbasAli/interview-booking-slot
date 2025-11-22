1ab16d1d45c75299d4c5909bbe1827b92cd1825e
ganesh23101979 <ganesh.santhosh201@gmail.com>
2025-11-23 01:17:54 +0530

    interview-booking

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,12 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
+// Load API URL from .env
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
+// Attach Token Automatically
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
